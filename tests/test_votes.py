b4c84cfc3662e2e4da5b08d163538442b00c3fad
chonlanotr <chonlanotr@gmail.com>
2023-11-04 13:41:25 +0700

    github action

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -21,3 +21,32 @@ def test_vote_twice_post(authorized_client, test_posts, test_vote):
     res = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1})
     assert res.status_code == 409
+
+def test_vote_delete_vote(authorized_client, test_posts, test_vote):
+    payload =  {"post_id": test_posts[3].id, "dir": 0}
+    res = authorized_client.post(
+        "/vote/", json=payload
+    )
+    assert res.status_code == 201
+
+def test_delete_vote_non_exist(authorized_client, test_posts):
+    payload =  {"post_id": test_posts[3].id, "dir": 0}
+    res = authorized_client.post(
+        "/vote/", json=payload
+    )
+    assert res.status_code == 404
+
+
+def test_vot_post_non_exist(authorized_client, test_posts):
+    payload =  {"post_id": 800000, "dir": 1}
+    res = authorized_client.post(
+        "/vote/", json=payload
+    )
+    assert res.status_code == 404
+
+def test_vote_unauthorized_user (client, test_posts):
+    payload =  {"post_id": test_posts[3].id, "dir": 1}
+    res = client.post(
+        "/vote/", json=payload
+    )
+    assert res.status_code == 401
