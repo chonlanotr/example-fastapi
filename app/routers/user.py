9ea4c20ab1af0c6a0a677e99b655238430dc5437
chonlanotr <chonlanotr@gmail.com>
2023-11-11 05:57:19 +0700

    deauthorize

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,10 +12,9 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    #hash the password - user.password
+    # hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
-
 
     new_user = models.User(**user.model_dump(mode="unchanged"))
     db.add(new_user)
@@ -24,16 +23,19 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+
 @router.get("/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id} does not exist.")
     return user
 
 
 @router.get("/", response_model=List[schemas.UserOut])
-def get_users(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-    users =  db.query(models.User).filter(
-            models.User.email.contains(search)).limit(limit).offset(skip).all()
+def get_users(db: Session = Depends(get_db), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    # def get_users(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    users = db.query(models.User).filter(
+        models.User.email.contains(search)).limit(limit).offset(skip).all()
     return users
